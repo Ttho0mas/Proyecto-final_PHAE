de8f09e5ff563b778f55b96047533fc82aee4bbf
Ttho0mas <164171714+Ttho0mas@users.noreply.github.com>
2024-06-16 11:00:44 -0300

    Add files via upload
    mi parte de la primera entrega

---
+++ PHAE/PintureriaArcoiris/categorias-productos/accesorios.php
@@ -0,0 +1,88 @@
+<?php  
+  include 'C:\xampp\htdocs\PHAE\PintureriaArcoiris\encabezado\encabezado.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ACCESORIOS</title>
+    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="\PHAE\PintureriaArcoiris\encabezado\estilo-e.css">
+    <script src="\PHAE\PintureriaArcoiris\encabezado\scripts.js"></script>
+    <link rel="stylesheet" href="\PHAE\PintureriaArcoiris\footer\estilo-p.css">
+
+    <link rel="icon" href="C:\xampp\htdocs\PHAE\PintureriaArcoiris\icon-piar.png" type="image/x-icon">
+</head>
+<body>
+    <div class="categorias">
+    <main>
+        <h1>ACCESORIOS:</h1>
+        <p>Seleccione una subcategoría que se adecue al producto que está buscando.</p>
+
+        <div class="seccion-subcategoria">
+            <div class="subcategoria">
+               <a href="#">  <div class="colorcont"></div></a>
+               <p>Pinceles</p>
+            </div>
+
+            <div class="subcategoria">
+                <a href="#"> <div class="colorcont"></div></a>
+                 <p>Rodillos</p>
+            </div>
+
+            <div class="subcategoria">
+                <a href="#"> <div class="colorcont"></div></a>
+                <p>Pinceletas</p>
+            </div>
+
+           
+        </div>
+
+        <div class="seccion-subcategoria">
+            <div class="subcategoria">
+                <a href="#"> <div class="colorcont"></div></a>
+                <p>Cintas de Enmascarar</p>
+            </div>
+
+            <div class="subcategoria">
+                <a href="#">  <div class="colorcont"></div></a>
+                <p>Cubetas y Bandejas</p>
+            </div>
+
+            <div class="subcategoria">
+                <a href="#"><div class="colorcont"></div></a>
+                <p>Lijas</p>
+            </div>
+
+           
+        </div>
+        <div class="seccion-subcategoria">
+            <div class="subcategoria">
+                <a href="#"> <div class="colorcont"></div></a>
+                <p>Espátulas</p>
+            </div>
+
+            <div class="subcategoria">
+                <a href="#"> <div class="colorcont"></div></a>
+                <p>Escaleras</p>
+            </div>
+
+            <div class="subcategoria">
+                <a href="#"> <div class="colorcont"></div></a>
+                <p>Otros accesorios</p>
+            </div>
+
+           
+        </div>
+                
+      
+       
+    </main>
+    </div>
+
+</body>
+</html>
+<?php  
+ include 'C:\xampp\htdocs\PHAE\PintureriaArcoiris\footer\piedepagina.php';
+?>
---
+++ PHAE/PintureriaArcoiris/index.php
@@ -0,0 +1,22 @@
+<?php  
+  include 'C:\xampp\htdocs\PHAE\PintureriaArcoiris\encabezado\encabezado.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pintureria Arcoiris | Inicio</title>
+    <link rel="stylesheet" href="\PHAE\PintureriaArcoiris\encabezado\estilo-e.css">
+    <script src="\PHAE\PintureriaArcoiris\encabezado\scripts.js"></script>
+    <link rel="stylesheet" href="\PHAE\PintureriaArcoiris\footer\estilo-p.css">
+
+    <link rel="icon" href="C:\xampp\htdocs\PHAE\PintureriaArcoiris\icon-piar.png" type="image/x-icon">
+</head>
+<body>
+    
+</body>
+</html>
+<?php  
+ include 'C:\xampp\htdocs\PHAE\PintureriaArcoiris\footer\piedepagina.php';
+?>
