de86753b4580fb4b17a24735a22a1456be779da2
Ttho0mas <164171714+Ttho0mas@users.noreply.github.com>
2024-06-19 19:46:59 -0300

    aca esta arreglado lo que estaba mal conectadp

---
+++ PintureriaArcoiris/login-registro/pruebaindex.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba index para login</title>
+</head>
+<body>
+    <h1> Felicidades has logrado conectar la bases de datos</h1>
+  <img src="tablasusuario.png" alt="" >
+
+</body>
+</html>
