498bc485e245b9c35b4eff70ae4e23b8ed570535
Ttho0mas <164171714+Ttho0mas@users.noreply.github.com>
2024-07-14 18:19:20 -0300

    Update contenido.php

--- PintureriaArcoiris/contenido_empresa/contenido.php
+++ PintureriaArcoiris/contenido_empresa/contenido.php
@@ -1,5 +1,5 @@
 <?php  
-  include 'C:\xampp\htdocs\PHAE\PintureriaArcoiris\encabezado\encabezado.php';
+  include '\PintureriaArcoiris\encabezado\encabezado.php';
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -7,9 +7,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sección de Publicidad y Datos de la Empresa</title>
-    <link rel="stylesheet" href="\PHAE\PintureriaArcoiris\encabezado\estilo-e.css">
-    <script src="\PHAE\PintureriaArcoiris\encabezado\scripts.js"></script>
-    <link rel="stylesheet" href="\PHAE\PintureriaArcoiris\footer\estilo-p.css">
+    <link rel="stylesheet" href="\PintureriaArcoiris\encabezado\estilo-e.css">
+    <script src="\PintureriaArcoiris\encabezado\scripts.js"></script>
+    <link rel="stylesheet" href="\PintureriaArcoiris\footer\estilo-p.css">
     <link rel="stylesheet" href="estilo-conteempresa.css">
 </head>
 <body>
@@ -23,15 +23,15 @@
         <p>Somos Pinturería Arcoiris, tu compañero creativo en el mundo de la pintura. Desde herramientas de alta calidad hasta una amplia gama de pinturas para cualquier proyecto, estamos aquí para hacer realidad tus ideas y transformar tus espacios. Ya sea que estés renovando tu hogar o embelleciendo un lugar comercial, en Pinturería Arcoiris encontrarás todo lo que necesitas para llevar tus proyectos al siguiente nivel.</p>
         <hr>
         <div class="info-contacto">
-            <img src="\PHAE\PintureriaArcoiris\iconos\ubicacion.png" alt="Dirección">
+            <img src="\PintureriaArcoiris\iconos\ubicacion.png" alt="Dirección">
             <p><strong>Dirección:</strong> Salto, Uruguay 2297</p>
         </div>
         <div class="info-contacto">
-            <img src="\PHAE\PintureriaArcoiris\iconos\horarios.png" alt="Teléfono">
+            <img src="\PintureriaArcoiris\iconos\horarios.png" alt="Teléfono">
             <p><strong>Teléfono:</strong> *(+598)...*</p>
         </div>
         <div class="info-contacto">
-            <img src="\PHAE\PintureriaArcoiris\iconos\correo-electronico.png" alt="Correo Electrónico">
+            <img src="\PintureriaArcoiris\iconos\correo-electronico.png" alt="Correo Electrónico">
             <p><strong>Correo Electrónico:</strong> *****@gmail.com</p>
         </div>
     </div>
@@ -40,5 +40,5 @@
 </body>
 </html>
 <?php  
- include 'C:\xampp\htdocs\PHAE\PintureriaArcoiris\footer\piedepagina.php';
+ include '\PintureriaArcoiris\footer\piedepagina.php';
 ?>
